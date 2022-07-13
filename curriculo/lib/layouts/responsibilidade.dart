4d167697fab6f25a1269a010a9faf37e496561f9
akinase <wesleyrdsouza@hotmail.com>
2022-07-13 08:02:08 -0300

    Create responsibilidade.dart

---
+++ curriculo/lib/layouts/responsibilidade.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Responsividade extends StatelessWidget {
+  final Widget mobilebody;
+  final Widget desktopApp;
+
+  Responsividade(
+      {required this.mobilebody,
+      required this.desktopApp}); //Construtor das classes mobile e desktop
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobilebody;
+        } else {
+          return desktopApp;
+        }
+      },
+    );
+  }
+}
